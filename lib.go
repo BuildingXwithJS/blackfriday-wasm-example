ebfd1f4870dfa521d4b777c06e0bc523e91ae691
Tim Ermilov <yamalight@gmail.com>
2018-07-04 19:20:34 +0200

    Expose basic callable function from golang

--- lib.go
+++ lib.go
@@ -1,6 +1,19 @@
 package main
 
+import (
+	"fmt"
+	"syscall/js"
+)
+
 func main() {
-	println("Hello world!")
+	c := make(chan struct{}, 0)
+
+	js.Global().Set("blackfridayFormat", js.NewCallback(format))
+
+	<-c
+	fmt.Println("Bye Wasm !")
 }
 
+func format(input []js.Value) {
+	fmt.Println("Format hello")
+}
